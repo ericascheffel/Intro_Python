efb1a855f82505fc8117dd9eea759c5e5791d983
ellesimas <ellesimas@gmail.com>
2020-10-19 18:10:18 -0300

    ADD_Desafios.py

---
+++ Turtle/Desafios.py
@@ -0,0 +1,32 @@
+"""Bem-vindo à sala de desafios!
+
+   Você pode salvar todo o seu progresso aqui também!
+   Organize está página como quiser!
+
+   Desafie-se!
+"""
+######################################
+# TÓPICOS: Sintaxe, padrões e Funções
+######################################
+
+"""
+DESAFIO 1: Crie algo com o turtle:
+"""
+
+"""
+DESAFIO 2: Relembre o uso de padrões usando a ITERAÇÃO para criar um desenho/figura/forma:
+"""
+
+"""
+DESAFIO 4: Crie uma função que possibilite criar desenhos|figuras|formas com base em alguns argumentos 
+"""
+
+"""
+DESAFIO 3: Crie uma função que ao ser chamada crie uma 'ÁRVORE'.
+
+Dica1: Fractal (do latim fractu: fração, quebrado) é uma figura da geometria não clássica muito encontrada 
+       na natureza, isto é, um objeto em que suas partes separadas repetem os traços (a aparência) 
+       do todo completo (padrão repetitivo)
+Dica2: https://aidobonsai.files.wordpress.com/2011/10/fractaltree.jpg
+Dica3: https://3.bp.blogspot.com/-vX49qkQWs20/Tud4BfpN2tI/AAAAAAAAAXA/5r-PkWbqGeU/s1600/%25C3%2581rvore.jpg
+"""
