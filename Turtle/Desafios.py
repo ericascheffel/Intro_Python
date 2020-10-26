7c466e0c6ef5872861a198f34c40be2d634ec219
ellesimas <ellesimas@gmail.com>
2020-10-26 18:46:41 -0300

    UPDATE_Desafio

--- Turtle/Desafios.py
+++ Turtle/Desafios.py
@@ -28,6 +28,7 @@ DESAFIO 4: Crie uma função que ao ser chamada crie uma 'ÁRVORE'.
 Dica1: Fractal (do latim fractu: fração, quebrado) é uma figura da geometria não clássica muito encontrada 
        na natureza, isto é, um objeto em que suas partes separadas repetem os traços (a aparência) 
        do todo completo (padrão repetitivo)
-Dica2: https://aidobonsai.files.wordpress.com/2011/10/fractaltree.jpg
-Dica3: https://3.bp.blogspot.com/-vX49qkQWs20/Tud4BfpN2tI/AAAAAAAAAXA/5r-PkWbqGeU/s1600/%25C3%2581rvore.jpg
+Dica2: https://pt.wikipedia.org/wiki/Recursividade_(Ci%C3%AAncia_da_Computa%C3%A7%C3%A3o)
+Dica3: https://aidobonsai.files.wordpress.com/2011/10/fractaltree.jpg
+Dica4: https://3.bp.blogspot.com/-vX49qkQWs20/Tud4BfpN2tI/AAAAAAAAAXA/5r-PkWbqGeU/s1600/%25C3%2581rvore.jpg
 """
