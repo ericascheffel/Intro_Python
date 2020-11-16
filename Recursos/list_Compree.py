0f3e4ed32a0c0f31097e3f7c072b7ee22c80a16f
Emanuelle Simas <ellesimas@gmail.com>
2020-11-16 14:09:52 -0300

    Update list_Compree.py

--- Recursos/list_Compree.py
+++ Recursos/list_Compree.py
@@ -3,10 +3,10 @@
 LISTA_BASE= list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle kyicrpylq() gq pcamkkclbcb lmu ynnjw ml rfc spj") 
 STRG_BASE = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle kyicrpylq() gq pcamkkclbcb lmu ynnjw ml rfc spj"
 
-list_chave =[chr(letra) for letra in range(ord('a'), ord('z')+1)]
+list_chave =[chr(letra) for letra in range(ord('a'), ord('z')+1)] # gera o alfabeto
 print(list_chave)
 
-list_valor =[chr(letra+2) for letra in range(ord('a'), ord('z')+1)] # gera o alfabeto
+list_valor =[chr(letra+2) for letra in range(ord('a'), ord('z')+1)] # progressão alfabéticaalterada para +2
 list_valor.remove("|")
 list_valor.remove("{")
 list_valor.extend("a")
@@ -25,12 +25,7 @@ print("Traduzindo:", EXTRA.translate(TRADUCAO))
 print("Traduzindo:", STRG_BASE.translate(TRADUCAO))
 
 
-
-
-#teste = [chr(letra) for letra in range(ord('a'),ord('z')+2)]
-#print(teste)
-
-
+# MODELO CONSTRUÇÃO SO ALFABETO
 """
 ALFABETO =[]
 # list de a-z
